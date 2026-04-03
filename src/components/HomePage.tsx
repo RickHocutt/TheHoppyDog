e53a5c3073e042f25137a1d38a4c3a3186e70289
Rick Hocutt <rick.hocutt@gmail.com>
2026-04-03 07:39:47 -0400

    Improve Home Page hero

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -18,18 +18,18 @@ export function HomePage() {
 
         <div className="relative z-10 text-center text-white max-w-5xl mx-auto px-4 mt-10">
 
-          <div className="flex justify-center items-center mb-40">
+          <div className="flex justify-center items-center flex-col md:flex-row mb-4 md:mb-40">
             <img src={mainLogo} className="w-40 h-40 rounded-full block mr-6" />
             <h1 className="text-4xl lg:text-6xl font-bold mb-8 drop-shadow-2xl" style={{ fontFamily: "'Original Surfer', sans-serif" }}>
               Welcome to <span className="text-amber-400"> <br/> The Hoppy Dog</span>
             </h1>
           </div>
 
-          <div className="bg-black bg-opacity-50 mt-8 p-8 rounded-2xl">
+          <div className="bg-black bg-opacity-50 my-8 p-8 rounded-2xl">
             <p className="text-2xl lg:text-4xl font-bold mb-4">Coming Soon!</p>
 
             <p className="text-xl text-gray-200 mb-8 leading-relaxed drop-shadow-lg">
-              New Hampshire's only indoor dog park &amp; taproom serving local craft beer and wine
+              SouthernNew Hampshire's only indoor dog park &amp; taproom serving local craft beer and wine
             </p>
 
             <p className="text-gray-300 mb-12 max-w-3xl mx-auto leading-relaxed">
